b5bdb2c610e3ea3e69e910e381aaf53c5cd939e1
romul00 <ramil-yahin@mail.ru>
2025-06-10 10:27:18 +0400

    #

---
+++ lesson_1_task.py
@@ -0,0 +1,2 @@
+my_name = 'Ramil'
+print (my_name)
---
+++ lesson_1_task_7.py
@@ -0,0 +1,81 @@
+def advert1():
+    print(8, end='')
+    advert2()
+  
+def advert2():
+    print(8, end='')
+    advert3()
+
+def advert3():
+    print(0, end='')
+    advert4()
+
+def advert4():
+    print(0, end='')
+    advert5()
+
+def advert5():
+    print(5, end='')
+    advert6()
+
+def advert6():
+    print(5, end='')
+    advert7()
+
+def advert7():
+    print(5, end='')
+    advert8()
+
+def advert8():
+    print(3, end='')
+    advert9()
+
+def advert9():
+    print(5, end='')
+    advert0()
+
+def advert0():
+    print(3, end='5')
+advert1()
+
+#если нужно в столбик, то можно сделать так
+# def advert1():
+#     print(8)
+#     advert2()
+  
+# def advert2():
+#     print(8)
+#     advert3()
+
+# def advert3():
+#     print(0)
+#     advert4()
+
+# def advert4():
+#     print(0)
+#     advert5()
+
+# def advert5():
+#     print(5)
+#     advert6()
+
+# def advert6():
+#     print(5)
+#     advert7()
+
+# def advert7():
+#     print(5)
+#     advert8()
+
+# def advert8():
+#     print(3)
+#     advert9()
+
+# def advert9():
+#     print(5)
+#     advert0()
+
+# def advert0():
+#     print(3)
+# advert1()
+# print(5)
