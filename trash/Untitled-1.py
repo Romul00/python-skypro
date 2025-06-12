c4d9eea15efbb71b0086b5f8c373cd0ebf2a6874
Romul00 <ramil-yahin@mail.ru>
2025-06-12 04:48:08 +0400

    #

--- trash/Untitled-1.py
+++
@@ -1,9 +0,0 @@
-age = int(input('ur age?'))
-
-if (age < 18):
-    print('сочка въеби малой')
-else: 
-    print('pivo)')
-
-if (age == 17):
-    print('wait 1 year')
--- trash/x = 1.py
+++
@@ -1,5 +0,0 @@
-students = ['Shanie', 'Rosanna', 'Alessandro', 'Mittie', 'Torrey', 'Jermaine', 'Reta', 'daun']
-
-for i in range(0, (len(students))):
-     print(students[i])
-
